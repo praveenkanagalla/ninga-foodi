61189e66a4d0640081596ff39576e6ef4dc57348
praveenkanagalla <praveenkumarkanagalla@gmail.com>
2021-04-22 13:04:15 +0530

    added js file

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    console.log("Praveen Kanagalla");
+    var myLinks = document.getElementById("myLinks");
+    if (myLinks.style.display === "block") {
+        myLinks.style.display = "none";
+    } else {
+        myLinks.style.display = "block";
+    }
+  }
